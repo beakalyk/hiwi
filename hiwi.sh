28f4b247a7bb8c11cedecccae2ee7fd5f67a8c1d
beakalyk <50494009+beakalyk@users.noreply.github.com>
2020-10-26 22:56:17 +0300

    Update hiwi.sh

--- hiwi.sh
+++ hiwi.sh
@@ -1,5 +1,9 @@
-#1.Write a Ruby or Bash script that will print usernames of all users on a Linux system together with their home directories.
-#for all created usernames
-getent passwd {1000..6000}| cut -d: -f1,6
-#for all usernames
-getent passwd {0..65535} | cut -d: -f1,6
+#!/bin/bash
+# # create folder named current_users in /var/log/ if the folder don't exist
+mkdir -p /var/log/current_users
+# create folder named user_changes in /var/log/ if the folder don't exist
+mkdir -p /var/log/user_changes
+# used only to check if the right combination of data is generated from the paswd file 
+getent passwd {1000..6000}| cut -d: -f1,6  > /var/log/current_users/Raw_current_users
+#Actual file generated with MD5 hash algorithm by the over writing    
+getent passwd {1000..6000}| cut -d: -f1,6 |md5sum >/var/log/current_users/current_users
